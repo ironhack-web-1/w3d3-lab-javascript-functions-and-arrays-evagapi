922b29f87a40b74b24c66b364bbc4698db0c8e87
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-07-23 18:17:48 +0200

    refactor: delete duplicated function

--- src/iteration-4.js
+++ src/iteration-4.js
@@ -1,14 +1,6 @@
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 
-function sumNumbers(arrayOfNumbers) {
-  let result = 0;
-  for (let number of arrayOfNumbers) {
-    result += number;
-  }
-  return result;
-}
-
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
 function averageNumbers(arrayOfNumbers) {
