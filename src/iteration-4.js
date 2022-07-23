c2f45c19e659b87049d78a8c1b6e568c22e89c1e
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-07-23 17:33:27 +0200

    refactor: declare function before using it

--- src/iteration-4.js
+++ src/iteration-4.js
@@ -1,6 +1,14 @@
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 
+function sumNumbers(arrayOfNumbers) {
+  let result = 0;
+  for (let number of arrayOfNumbers) {
+    result += number;
+  }
+  return result;
+}
+
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
 function averageNumbers(arrayOfNumbers) {
@@ -13,14 +21,6 @@ function averageNumbers(arrayOfNumbers) {
   }
 
   return sumNumbers(arrayOfNumbers) / arrayOfNumbers.length;
-}
-
-function sumNumbers(arrayOfNumbers) {
-  let result = 0;
-  for (let number of arrayOfNumbers) {
-    result += number;
-  }
-  return result;
 }
 
 // Level 2: Array of strings
